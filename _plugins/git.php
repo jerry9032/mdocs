230f9b425f6c604335c9d8366adcdbeae4632663
ksarch-store <ksarch-store@baidu.com>
2013-04-20 02:10:01 +0800

    use absolute git path in jumbo, as php can't find git in $PATH

--- _plugins/git.php
+++ _plugins/git.php
@@ -2,34 +2,35 @@
 $_git_flock_fp = NULL;
 function _git_after_save_edit($file) {
     global $_git_flock_fp;
+    $git = "~/.jumbo/bin/git";
     if (!is_dir('_doc/.git')) {
         return;
     }
-    file_put_contents('aaaaa', $file);
+    //file_put_contents('aaaaa', $file);
     $file = substr($file, 5);
     $retcode = 0;
     $output = array();
     $f = escapeshellarg($file);
-    if (trim(shell_exec("cd _doc && git status --porcelain $f | wc -l")) === "0") {
+    if (trim(shell_exec("cd _doc && $git status --porcelain $f | wc -l")) === "0") {
         return;
     }
     exec("cd _doc \\
-        && git checkout -B tmpbranch \\
-        && git add $f \\
-        && git commit -m 'Modify file '$file \\
-        && git checkout master \\
-        && git fetch \\
-        && git reset --hard origin/master \\
-        && git checkout tmpbranch \\
-        && git rebase master \\
-        && git checkout master \\
-        && git merge tmpbranch \\
-        && git branch -d tmpbranch \\
-        && git push origin master
+        && $git checkout -B tmpbranch \\
+        && $git add $f \\
+        && $git commit -m 'Modify file '$file \\
+        && $git checkout master \\
+        && $git fetch \\
+        && $git checkout tmpbranch \\
+        && $git rebase master \\
+        && $git checkout master \\
+        && $git merge tmpbranch \\
+        && $git branch -d tmpbranch \\
+        && $git push origin master
         ", $output, $retcode);
+    #&& $git reset --hard origin/master \\ # after fetch
     if ($retcode != 0) {
-        exec("cd _doc && git rebase --abort");
-        exec("cd _doc && git checkout master && git reset --hard");
+        exec("cd _doc && $git rebase --abort");
+        exec("cd _doc && $git checkout master && $git reset --hard");
         echo "Commit to git failed, maybe conflict\n";
         flock($_git_flock_fp, LOCK_UN);
         return ACTION_ABORT;
@@ -39,6 +40,7 @@ function _git_after_save_edit($file) {
 
 function _git_before_save_edit() {
     global $_git_flock_fp;
+    $git = "~/.jumbo/bin/git";
     if (!is_dir('_doc/.git')) {
         return;
     }
@@ -51,10 +53,12 @@ function _git_before_save_edit() {
 }
 
 function _git_sync() {
+    $git = "~/.jumbo/bin/git";
     if (!is_dir('_doc/.git')) {
         return;
     }
-    exec("cd _doc && git fetch && git reset --hard origin/master");
+    exec("cd _doc && $git fetch");
+    #exec("cd _doc && $git fetch && $git reset --hard origin/master");
 }
 
 add_action('after_save_edit', '_git_after_save_edit');
